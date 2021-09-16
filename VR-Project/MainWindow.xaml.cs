881788639908f3e164bb2ef76c2f33af0b05b51e
jasper baijens <jasperbayens@outlook.com>
2021-09-16 16:20:08 +0200

    Auto stash before merge of "JsonBranch" and "origin/JsonBranch"

--- VR-Project/MainWindow.xaml.cs
+++ VR-Project/MainWindow.xaml.cs
@@ -109,11 +109,20 @@ namespace VR_Project
             //client.Close();
             //client.Dispose();
 
-           
 
-       
 
+
+            Debug.WriteLine("Creating skybox");
             Skybox skybox = new Skybox();
+            skybox.id = "scene/skybox/update";
+            skybox.setType(Skybox.SkyboxType.DYNAMIC);
+            messageToSend = WrapMessage(Encoding.ASCII.GetBytes(WrapJsonMessage<Skybox>(dest, skybox)));
+            client.GetStream().Write(messageToSend, 0, messageToSend.Length);
+            client.GetStream().Flush();
+            received = ReadMessage(client);
+            string receivedMessage = Encoding.ASCII.GetString(received);
+            Debug.WriteLine(receivedMessage);
+
             skybox.id = "scene/skybox/settime";
             skybox.data.time = 24;
 
