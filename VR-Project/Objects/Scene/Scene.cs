df435398bd9e57cae6df33f62c1a91b10e9c7aab
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2021-09-16 15:26:53 +0200

    Auto stash before merge of "JsonBranch" and "origin/JsonBranch"

--- VR-Project/Objects/Scene/Scene.cs
+++ VR-Project/Objects/Scene/Scene.cs
@@ -1,9 +1,29 @@
 ﻿using System;
 
-public class Scene 
+public class Scene
 {
-	public Scene()
+	public string id;
+	public SceneData data;
+
+	public Scene(string id)
 	{
+		this.id = id;
+		this.data = new SceneData();
 	}
 
-}
+	public void setFileName(string fileName)
+    {
+		this.data.filename = fileName;
+    }
+
+	public void setOverwrite(bool overwrite)
+	{
+		this.data.overwrite = overwrite;
+	}
+
+	public class SceneData
+	{
+		#nullable enable
+		public string? filename { get; set; }
+		public bool? overwrite { get; set; }
+	}
