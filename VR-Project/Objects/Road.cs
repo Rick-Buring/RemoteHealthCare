be437a3f83e07db4a2c1fea7607d6314553e38cf
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2021-09-16 15:03:05 +0200

    RoadData format

--- VR-Project/Objects/Road.cs
+++ VR-Project/Objects/Road.cs
@@ -1,9 +1,55 @@
 ﻿using System;
 
-public class Road : VRObject
+public class Road 
 {
-	public Road()
+	public string id;
+	public RoadData data;
+
+	public Road(string id, string route)
 	{
+		this.id = id;
+		this.data = new RoadData(route);
+	}
+
+	public void SetRoute(string route)
+    {
+		this.data.route = route;
+    }
+
+	public void SetDiffuse(string diffuse)
+	{
+		this.data.diffuse = diffuse;
+	}
+
+	public void SetNormal(string normal)
+	{
+		this.data.normal = normal;
+	}
+
+	public void SetSpecular(string specular)
+	{
+		this.data.specular = specular;
+	}
+
+	public void SetHeightOffSet(double heightoffset)
+	{
+		this.data.heightoffset = heightoffset;
+	}
+
+	public class RoadData
+    {
+		public RoadData(string route)
+        {
+			this.route = route;
+        }
+
+		public string route { get; set; }
+
+		#nullable enable
+		public string? diffuse { get; set; }
+		public string? normal { get; set; }
+		public string? specular { get; set; }
+		public double? heightoffset { get; set; }
 	}
 
 }
