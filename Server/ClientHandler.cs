dc6a0e2b9e624ac2b4d5ee25205cff0a0e3d379e
JochemDG <Pipsquek542>
2021-09-22 14:26:14 +0200

    Created server classes

---
+++ Server/ClientHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Server
+{
+    public class ClientHandler
+    {
+        public ClientHandler()
+        {
+        }
+    }
+}
---
+++ Server/DataManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Server
+{
+    public class DataManager
+    {
+        public DataManager()
+        {
+        }
+    }
+}
