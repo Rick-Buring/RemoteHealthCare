2dec9855d8cc20c417d43df866185b9caa85bd3c
jasper baijens <jasperbayens@outlook.com>
2021-09-22 15:09:05 +0200

    Added mvvm principle to the vr application.

---
+++ VR-Project/ViewModel.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Input;
+
+using VR_Project.Util;
+
+namespace VR_Project
+{
+    class ViewModel : ObservableObject, EngineCallback
+    {
+
+        private VrManager vrManager;
+
+        private ICommand _selectEngine;
+
+
+        public ICommand SelectEngine
+        {
+            get
+            {
+
+                if (_selectEngine == null)
+                {
+                    _selectEngine = new RelayCommand(param => this.button_Click());
+                }
+
+                return _selectEngine;
+            }
+        }
+
+
+        public ViewModel()
+        {
+            this.vrManager = new VrManager(this);
+        }
+
+
+
+
+        private VrManager.Data _selectedMilight;
+
+        public VrManager.Data SelectedMilight
+        {
+            get { return _selectedMilight; }
+            set
+            {
+                _selectedMilight = value;
+            }
+        }
+        private void button_Click()
+        {
+            //if (SelectedMilight == null)
+            //    return;
+            //this.tunnelID = SelectedMilight.id;
+            if (SelectedMilight == null)
+                return;
+            this.vrManager.connectToTunnel(SelectedMilight.id);
+            //connectToTunnel();
+
+        }
+        public ObservableCollection<VrManager.Data> ob { get; set; }
+        public void notify(ObservableCollection<VrManager.Data> ob)
+        {
+            this.ob = ob;
+        }
+    }
+}
---
+++ VR-Project/VrManager.cs
@@ -0,0 +1,315 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace VR_Project
+{
+    class VrManager
+    {
+
+        private TcpClient client = new TcpClient("145.48.6.10", 6666);
+        private string dest;
+
+        public VrManager(EngineCallback listener)
+        {
+         
+            Root t = new Root();
+            t.id = "session/list";
+            //string message = @"{""id"" : ""session/list""}";
+            string message = JsonConvert.SerializeObject(t);
+            //Debug.WriteLine(message);
+            byte[] messageArray = Encoding.ASCII.GetBytes(message);
+            var messageToSend = WrapMessage(messageArray);
+            client.GetStream().Write(messageToSend, 0, messageToSend.Length);
+            client.GetStream().Flush();
+            //writer.Write(, 0);
+            //writer.Flush();
+            //Console.WriteLine(reader.ReadToEnd());
+            byte[] array = new byte[4];
+
+
+            client.GetStream().Read(array, 0, 4);
+
+            //int line = reader.Read();
+            int size = BitConverter.ToInt32(array);
+            byte[] received = new byte[size];
+
+
+            int bytesRead = 0;
+            while (bytesRead < size)
+            {
+                int read = client.GetStream().Read(received, bytesRead, received.Length - bytesRead);
+                bytesRead += read;
+                Console.WriteLine("ReadMessage: " + read);
+            }
+
+            //client.GetStream().Read(received, 0, size);
+
+            string test = Encoding.ASCII.GetString(received);
+            //Debug.WriteLine(test);
+            root = JsonConvert.DeserializeObject<Root>(test);
+
+            ObservableCollection<Data> ob = new ObservableCollection<Data>();
+
+            foreach (Data d in root.data)
+                ob.Add(d);
+
+            listener.notify(ob);
+
+        }
+
+        //private void MainWindow_Closed(object sender, EventArgs e)
+        //{
+        //    Debug.WriteLine("Closing and disposing client");
+        //    this.client.Close();
+        //    this.client.Dispose();
+        //    Debug.WriteLine("Closed and disposed client");
+        //}
+
+        private Root root;
+        //public ObservableCollection<Data> ob { get; set; }
+
+        private string tunnelID;
+        private Data _selectedMilight;
+
+        public Data SelectedMilight
+        {
+            get { return _selectedMilight; }
+            set
+            {
+                _selectedMilight = value;
+            }
+        }
+
+
+        public void connectToTunnel(string tunnelID)
+        {
+            string tunnel = @"{""id"" : ""tunnel/create"", ""data"" :{""session"" : """ + tunnelID + @"""}}";
+            byte[] messageToSend = WrapMessage(Encoding.ASCII.GetBytes(tunnel));
+            client.GetStream().Write(messageToSend, 0, messageToSend.Length);
+            client.GetStream().Flush();
+
+            byte[] received = ReadMessage(client);
+            string tunnelOpen = Encoding.ASCII.GetString(received);
+            Debug.WriteLine(tunnelOpen);
+
+            var destVar = JsonConvert.DeserializeObject(tunnelOpen);
+            this.dest = JObject.FromObject(JObject.Parse(tunnelOpen).GetValue("data")).GetValue("id").ToString();
+
+            changeSkyBoxTime(15);
+
+            deleteGroundPlane();
+
+            //addTerrain();
+
+            MakeAndFollowRoute(makeBikeObject());
+        }
+
+        public string makeBikeObject()
+        {
+
+            ObjectNode bikeNode = new ObjectNode("scene/node/add", "bike", @"data\NetworkEngine\models\cars\generic\blue.obj", new int[3] { 0, 0, 0 });
+
+            JObject BikeResponse;
+
+            SendMessageResponseToJsonArray(client, WrapJsonMessage<ObjectNode>(this.dest, bikeNode), out BikeResponse);
+
+            return BikeResponse.Value<JObject>("data").Value<JObject>("data").Value<JObject>("data").Value<string>("uuid");
+        }
+
+        public void MakeAndFollowRoute(string nodeID)
+        {
+            JObject routeResponse;
+            Route r = Route.getRoute();
+            SendMessageResponseToJsonArray(this.client, WrapJsonMessage<Route.RouteObject>(this.dest, r.addRoute(true)), out routeResponse);
+            string routeID = routeResponse.Value<JObject>("data").Value<JObject>("data").Value<JObject>("data").Value<string>("uuid");
+            Road road = new Road("scene/road/add", routeID);
+
+            SendMessage(client, WrapJsonMessage<Road>(dest, road));
+
+            SendMessage(client, WrapJsonMessage<Route.RouteObject>(dest, r.followRoute(routeID, nodeID, 2)));
+        }
+
+        public void changeSkyBoxTime(int time)
+        {
+            Skybox skybox = new Skybox();
+            skybox.id = "scene/skybox/settime";
+            skybox.data.time = time;
+
+            SendMessage(client, WrapJsonMessage<Skybox>(this.dest, skybox));
+
+            skybox.id = "scene/skybox/update";
+
+
+            SendMessage(client, WrapJsonMessage<Skybox>(this.dest, skybox));
+        }
+
+        public void deleteGroundPlane()
+        {
+            Node findNode = new Node("scene/node/find");
+            findNode.data.name = "GroundPlane";
+
+            JObject jObject;
+            SendMessageResponseToJsonArray(this.client, WrapJsonMessage<Node>(this.dest, findNode), out jObject);
+
+
+            string uuid = jObject.Value<JObject>("data").Value<JObject>("data").Value<JArray>("data")[0].Value<string>("uuid");
+
+            Node deleteNode = new Node("scene/node/delete");
+            deleteNode.data.id = uuid;
+
+            SendMessage(client, WrapJsonMessage<Node>(this.dest, deleteNode));
+        }
+
+        public void add3dObjects()
+        {
+            ObjectNode ObjectNode1 = new ObjectNode("scene/node/add", "object1", @"data\NetworkEngine\models\trees\fantasy\tree1.obj", new int[3] { 1, 2, 1 });
+
+            SendMessage(client, WrapJsonMessage<ObjectNode>(this.dest, ObjectNode1));
+
+            ObjectNode ObjectNode2 = new ObjectNode("scene/node/add", "object1", @"data\NetworkEngine\models\trees\fantasy\tree1.obj", new int[3] { 0, 3, 1 });
+
+            SendMessage(client, WrapJsonMessage<ObjectNode>(this.dest, ObjectNode2));
+
+            ObjectNode ObjectNode3 = new ObjectNode("scene/node/add", "object1", @"data\NetworkEngine\models\trees\fantasy\tree1.obj", new int[3] { 0, 0, 0 });
+
+            SendMessage(client, WrapJsonMessage<ObjectNode>(this.dest, ObjectNode3));
+        }
+
+        public void addTerrain()
+        {
+
+            Terrain terrain = new Terrain("scene/terrain/add", new int[] { 256, 256 }, Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "/Heightmap.txt");
+
+            SendMessage(client, WrapJsonMessage<Terrain>(this.dest, terrain));
+
+            TerrainNode node = new TerrainNode("scene/node/add", "terrainNode", true);
+
+            SendMessage(client, WrapJsonMessage<TerrainNode>(this.dest, node));
+        }
+
+        public static string WrapJsonMessage<T>(string dest, T t)
+        {
+            string objectToJson = JsonConvert.SerializeObject(t);
+
+            string message = @"{""id"" : ""tunnel/send"", ""data"" : " + @"{""dest"" : """ + dest + @""", ""data"" : " + objectToJson + "}}";
+
+            return message;
+        }
+
+        public static byte[] WrapMessage(byte[] message)
+        {
+            // Get the length prefix for the message
+            byte[] lengthPrefix = BitConverter.GetBytes(message.Length);
+            // Concatenate the length prefix and the message
+            byte[] ret = new byte[lengthPrefix.Length + message.Length];
+            lengthPrefix.CopyTo(ret, 0);
+            message.CopyTo(ret, lengthPrefix.Length);
+            return ret;
+        }
+
+
+
+        //public static string getCorrectID(Root root)
+        //{
+        //    foreach (Data d in root.data)
+        //    {
+        //        if (d.clientinfo.host.Equals(Environment.MachineName))
+        //        {
+        //            return d.id;
+        //        }
+        //    }
+        //    Debug.WriteLine("Could not find desktop");
+        //    return "0XFF";
+        //}
+
+        public static void SendMessage(TcpClient client, string message)
+        {
+            Debug.WriteLine(message);
+            byte[] messageToSend = WrapMessage(Encoding.ASCII.GetBytes(message));
+            client.GetStream().Write(messageToSend, 0, messageToSend.Length);
+            client.GetStream().Flush();
+            Debug.WriteLine(Encoding.ASCII.GetString(ReadMessage(client)));
+        }
+
+        public static void SendMessageResponseToJsonArray(TcpClient client, string message, out JObject jObject)
+        {
+            Debug.WriteLine(message);
+            byte[] messageToSend = WrapMessage(Encoding.ASCII.GetBytes(message));
+            client.GetStream().Write(messageToSend, 0, messageToSend.Length);
+            client.GetStream().Flush();
+            jObject = (JObject)JsonConvert.DeserializeObject(Encoding.ASCII.GetString(ReadMessage(client)));
+        }
+
+        public static byte[] ReadMessage(TcpClient client)
+        {
+            //Console.WriteLine(reader.ReadToEnd());
+            byte[] array = new byte[4];
+
+            client.GetStream().Read(array, 0, 4);
+
+            //int line = reader.Read();
+            int size = BitConverter.ToInt32(array);
+            byte[] received = new byte[size];
+
+
+            int bytesRead = 0;
+            while (bytesRead < size)
+            {
+                int read = client.GetStream().Read(received, bytesRead, received.Length - bytesRead);
+                bytesRead += read;
+                Console.WriteLine("ReadMessage: " + read);
+            }
+
+            return received;
+
+        }
+
+
+        public class Fp
+        {
+            public double? time { get; set; }
+            public double? fps { get; set; }
+        }
+
+        public class Clientinfo
+        {
+            public string? host { get; set; }
+            public string? user { get; set; }
+            public string? file { get; set; }
+            public string? renderer { get; set; }
+        }
+
+        public class Data
+        {
+            public string? id { get; set; }
+            public DateTime? beginTime { get; set; }
+            public DateTime? lastPing { get; set; }
+            public List<Fp>? fps { get; set; }
+            public List<string>? features { get; set; }
+            public Clientinfo? clientinfo { get; set; }
+        }
+
+        public class Root
+        {
+            public string? id { get; set; }
+            public List<Data>? data { get; set; }
+        }
+
+        //private void button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (SelectedMilight == null)
+        //        return;
+        //    this.tunnelID = SelectedMilight.id;
+            
+        //}
+
+    }
+}
