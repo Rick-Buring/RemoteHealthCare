e2768322e77d31cff2a24d1aa067bff40f10ddf0
JochemDG <Pipsquek542>
2021-09-22 14:47:22 +0200

    Entered body for Server and ClientHandler classes

--- Server/ClientHandler.cs
+++ Server/ClientHandler.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
 namespace Server
 {
     public class ClientHandler
     {
-        public ClientHandler()
+        private TcpClient tcpClient;
+        private NetworkStream stream;
+
+        public ClientHandler(TcpClient tcpClient)
         {
+            this.tcpClient = tcpClient;
+            this.stream = this.tcpClient.GetStream();
+        }
+
+        private void Read()
+        {
+            byte[] length = new byte[4];
+            this.stream.Read(length, 0, 4);
+
+            int size = BitConverter.ToInt32(length);
+
+            byte[] received = new byte[size];
+
+            int bytesRead = 0;
+            while (bytesRead < size)
+            {
+                int read = this.stream.Read(received, bytesRead, received.Length - bytesRead);
+                bytesRead += read;
+                Console.WriteLine("ReadMessage: " + read);
+            }
+
+            string result = Encoding.ASCII.GetString(received);
+        }
+
+        private void Parse(string toParse)
+        {
+            
         }
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
 
 namespace Server
 {
-    class Program
+    class Server
     {
+        private TcpListener listener;
+        private List<ClientHandler> clients;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Server();
+        }
+
+        public Server()
+        {
+            this.listener = new TcpListener(System.Net.IPAddress.Any, 5005);
+            listener.Start();
+            listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+        }
+
+        private void OnConnect(IAsyncResult ar)
+        {
+            var tcpClient = listener.EndAcceptTcpClient(ar);
+            Console.WriteLine($"Client connected from {tcpClient.Client.RemoteEndPoint}");
+            clients.Add(new ClientHandler(tcpClient));
+            listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
         }
     }
 }
