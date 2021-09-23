dcdfa3158aaa2166b10545c806e2178813bf73e0
R <r.buring@student.avans.nl>
2021-09-23 23:16:13 +0200

    created test application for encryption and this is working

---
+++ EncryptionTest/EncryptionTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+
+namespace EncryptionTest
+{
+    class Program
+    {
+        private static string certificateName; 
+        static void Main(string[] args)
+        {
+            certificateName = "";
+           
+            new Thread(startServer).Start();
+
+
+            string serverCertificateName = "testCertificaat";
+            string machineName = "";
+            Client.RunClient(machineName, serverCertificateName);
+
+        }
+
+        private static void startServer()
+        {
+            Server.RunServer(certificateName);
+        }
+
+       
+    }
+}
