4e50b48593a592ae3805a11ad36b7395e59fc298
R <r.buring@student.avans.nl>
2021-09-24 14:54:03 +0200

    implemented functions

--- EncryptionTest/EncryptionTest/Client.cs
+++ EncryptionTest/EncryptionTest/Client.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 
 namespace EncryptionTest
 {
     class Client
     {
         private static Hashtable certificateErrors = new Hashtable();
-
-        public static object SslTcpClient { get; private set; }
+        private static SslStream stream;
+        private static TcpClient client;
 
         // The following method is invoked by the RemoteCertificateValidationDelegate.
         public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
@@ -21,19 +23,19 @@ namespace EncryptionTest
             if (sslPolicyErrors == SslPolicyErrors.None)
                 return true;
 
-            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+            Debug.WriteLine("Certificate error: {0}", sslPolicyErrors);
 
             // Do not allow this client to communicate with unauthenticated servers.
             return false;
         }
-        public static void RunClient(string machineName, string serverName)
+        public static void RunClient(string machineName, string certificateName)
         {
             // Create a TCP/IP client socket.
             // machineName is the host running the server application.
-            TcpClient client = new TcpClient(machineName, 8080);
+            client = new TcpClient(machineName, 8080);
             Console.WriteLine("Client connected.");
             // Create an SSL stream that will close the client's stream.
-            SslStream sslStream = new SslStream(
+            stream = new SslStream(
                 client.GetStream(),
                 false,
                 new RemoteCertificateValidationCallback(ValidateServerCertificate),
@@ -42,64 +44,85 @@ namespace EncryptionTest
             // The server name must match the name on the server certificate.
             try
             {
-                sslStream.AuthenticateAsClient(serverName);
+                stream.AuthenticateAsClient(certificateName);
             }
             catch (AuthenticationException e)
             {
-                Console.WriteLine("Exception: {0}", e.Message);
+                Debug.WriteLine("Exception: {0}", e.Message);
                 if (e.InnerException != null)
                 {
                     Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
                 }
-                Console.WriteLine("Authentication failed - closing the connection.");
+                Debug.WriteLine("Authentication failed - closing the connection.");
                 client.Close();
                 return;
             }
-            // Encode a test message into a byte array.
-            // Signal the end of the message using the "<EOF>".
-            byte[] messsage = Encoding.UTF8.GetBytes("Hello from the client.<EOF>");
-            // Send hello message to the server.
-            sslStream.Write(messsage);
-            sslStream.Flush();
-            // Read message from the server.
-            string serverMessage = ReadMessage(sslStream);
-            Console.WriteLine("Server says: {0}", serverMessage);
-            // Close the client connection.
-            client.Close();
-            Console.WriteLine("Client closed.");
-        }
-        static string ReadMessage(SslStream sslStream)
-        {
-            // Read the  message sent by the server.
-            // The end of the message is signaled using the
-            // "<EOF>" marker.
-            byte[] buffer = new byte[2048];
-            StringBuilder messageData = new StringBuilder();
-            int bytes = -1;
-            do
+
+            new Thread(ReadMessages).Start();
+
+            while (true)
             {
-                bytes = sslStream.Read(buffer, 0, buffer.Length);
+                Write(Encoding.ASCII.GetBytes(Console.ReadLine()));
+            }
 
-                // Use Decoder class to convert from bytes to UTF8
-                // in case a character spans two buffers.
-                Decoder decoder = Encoding.UTF8.GetDecoder();
-                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
-                decoder.GetChars(buffer, 0, bytes, chars, 0);
-                messageData.Append(chars);
-                // Check for EOF.
-                if (messageData.ToString().IndexOf("<EOF>") != -1)
-                {
-                    break;
-                }
-            } while (bytes != 0);
-
-            return messageData.ToString();
         }
-        private static void DisplayUsage()
+
+        public static void Write(byte[] message)
         {
-            Console.WriteLine("To start the client specify:");
-            Console.WriteLine("clientSync machineName [serverName]");
-            Environment.Exit(1);
+            try
+            {
+                stream.Write(WrapMessage(message));
+                stream.Flush();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+        public static byte[] WrapMessage(byte[] message)
+        {
+            // Get the length prefix for the message
+            byte[] lengthPrefix = BitConverter.GetBytes(message.Length);
+            // Concatenate the length prefix and the message
+            byte[] ret = new byte[lengthPrefix.Length + message.Length];
+            lengthPrefix.CopyTo(ret, 0);
+            message.CopyTo(ret, lengthPrefix.Length);
+            return ret;
+        }
+        public static void ReadMessages()
+        {
+            while(true)
+            {
+                string received = Read(stream);
+                Console.WriteLine(received);
+                if (received == "BYE")
+                    close();
+            }
+        }
+
+        public static string Read(SslStream stream)
+        {
+            byte[] length = new byte[4];
+            stream.Read(length, 0, 4);
+
+            int size = BitConverter.ToInt32(length);
+
+            byte[] received = new byte[size];
+
+            int bytesRead = 0;
+            while (bytesRead < size)
+            {
+                int read = stream.Read(received, bytesRead, received.Length - bytesRead);
+                bytesRead += read;
+                //Console.WriteLine("ReadMessage: " + read);
+            }
+            return Encoding.ASCII.GetString(received);
+        }
+
+        private static void close()
+        {
+            stream.Close();
+            client.Close();
         }
     }
 
--- EncryptionTest/EncryptionTest/Server.cs
+++ EncryptionTest/EncryptionTest/Server.cs
@@ -42,16 +42,20 @@ namespace EncryptionTest
             {
                 sslStream.AuthenticateAsServer(serverCertificate, clientCertificateRequired: false, checkCertificateRevocation: true);
 
+                while (true)
+                {
+                    string messageData = Client.Read(sslStream);
+                    Console.WriteLine($"Server received: {messageData}");
 
-                // Read a message from the client.
-                Console.WriteLine("Waiting for client message...");
-                string messageData = ReadMessage(sslStream);
-                Console.WriteLine("Received: {0}", messageData);
-
-                // Write a message to the client.
-                byte[] message = Encoding.UTF8.GetBytes("Hello from the server.<EOF>");
-                Console.WriteLine("Sending hello message.");
-                sslStream.Write(message);
+                    // Write a message to the client.
+                    byte[] message = Encoding.ASCII.GetBytes(messageData);
+                    sslStream.Write(Client.WrapMessage(message));
+                    sslStream.Flush();
+                    if(messageData == "BYE")
+                    {
+                        break;
+                    }
+                }
             }
             catch (AuthenticationException e)
             {
@@ -73,88 +77,6 @@ namespace EncryptionTest
                 sslStream.Close();
                 client.Close();
             }
-        }
-        static string ReadMessage(SslStream sslStream)
-        {
-            // Read the  message sent by the client.
-            // The client signals the end of the message using the
-            // "<EOF>" marker.
-            byte[] buffer = new byte[2048];
-            StringBuilder messageData = new StringBuilder();
-            int bytes = -1;
-            do
-            {
-                // Read the client's test message.
-                bytes = sslStream.Read(buffer, 0, buffer.Length);
-
-                // Use Decoder class to convert from bytes to UTF8
-                // in case a character spans two buffers.
-                Decoder decoder = Encoding.UTF8.GetDecoder();
-                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
-                decoder.GetChars(buffer, 0, bytes, chars, 0);
-                messageData.Append(chars);
-                // Check for EOF or an empty message.
-                if (messageData.ToString().IndexOf("<EOF>") != -1)
-                {
-                    break;
-                }
-            } while (bytes != 0);
-
-            return messageData.ToString();
-        }
-        static void DisplaySecurityLevel(SslStream stream)
-        {
-            Console.WriteLine("Cipher: {0} strength {1}", stream.CipherAlgorithm, stream.CipherStrength);
-            Console.WriteLine("Hash: {0} strength {1}", stream.HashAlgorithm, stream.HashStrength);
-            Console.WriteLine("Key exchange: {0} strength {1}", stream.KeyExchangeAlgorithm, stream.KeyExchangeStrength);
-            Console.WriteLine("Protocol: {0}", stream.SslProtocol);
-        }
-        static void DisplaySecurityServices(SslStream stream)
-        {
-            Console.WriteLine("Is authenticated: {0} as server? {1}", stream.IsAuthenticated, stream.IsServer);
-            Console.WriteLine("IsSigned: {0}", stream.IsSigned);
-            Console.WriteLine("Is Encrypted: {0}", stream.IsEncrypted);
-        }
-        static void DisplayStreamProperties(SslStream stream)
-        {
-            Console.WriteLine("Can read: {0}, write {1}", stream.CanRead, stream.CanWrite);
-            Console.WriteLine("Can timeout: {0}", stream.CanTimeout);
-        }
-        static void DisplayCertificateInformation(SslStream stream)
-        {
-            Console.WriteLine("Certificate revocation list checked: {0}", stream.CheckCertRevocationStatus);
-
-            X509Certificate localCertificate = stream.LocalCertificate;
-            if (stream.LocalCertificate != null)
-            {
-                Console.WriteLine("Local cert was issued to {0} and is valid from {1} until {2}.",
-                    localCertificate.Subject,
-                    localCertificate.GetEffectiveDateString(),
-                    localCertificate.GetExpirationDateString());
-            }
-            else
-            {
-                Console.WriteLine("Local certificate is null.");
-            }
-            // Display the properties of the client's certificate.
-            X509Certificate remoteCertificate = stream.RemoteCertificate;
-            if (stream.RemoteCertificate != null)
-            {
-                Console.WriteLine("Remote cert was issued to {0} and is valid from {1} until {2}.",
-                    remoteCertificate.Subject,
-                    remoteCertificate.GetEffectiveDateString(),
-                    remoteCertificate.GetExpirationDateString());
-            }
-            else
-            {
-                Console.WriteLine("Remote certificate is null.");
-            }
-        }
-        private static void DisplayUsage()
-        {
-            Console.WriteLine("To start the server specify:");
-            Console.WriteLine("serverSync certificateFile.cer");
-            Environment.Exit(1);
         }
        
     }
