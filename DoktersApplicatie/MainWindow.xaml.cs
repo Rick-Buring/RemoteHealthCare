5a0c79352db68986aa0b244314416c605633bdfb
Leon van der Sar <leon.v.d.sar@gmail.com>
2021-10-06 16:12:21 +0200

    Message stuff works finally, and somehow you can bind events. Like wow

--- DoktersApplicatie/MainWindow.xaml.cs
+++ DoktersApplicatie/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,6 @@ namespace DoktersApplicatie
 
     public partial class MainWindow : Window
     {
-        
 
         private readonly ViewModel viewModel;
 
--- DoktersApplicatie/ViewModel.cs
+++ DoktersApplicatie/ViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Prism.Commands;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -7,39 +6,35 @@ using System.Text;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Prism.Mvvm;
+using Prism.Commands;
+using System.Collections.ObjectModel;
 
 namespace DoktersApplicatie
 {
     class ViewModel : BindableBase, INotifyPropertyChanged
     {
 
-        public DelegateCommand<object> MoreInfo { get; private set; }
+        public DelegateCommand<object> cMoreInfo { get; private set; }
+        public DelegateCommand<object> cStartStopSession { get; private set; }
+        public DelegateCommand<object> cEmergencyStop { get; private set; }
+        public DelegateCommand<object> cSendMessage { get; private set; }
 
-        public List<Employee> Employees { get; private set; }
+        public ObservableCollection<Client> Clients { get; private set; }
+        public ObservableCollection<Message> Messages { get; private set; }
         public int ActiveSecondaryTab { get; set; }
         public int ActiveMainTab { get; set; }
 
         public ViewModel()
         {
             Data data = new Data();
-            this.Employees = data.employees;
-            MoreInfo = new DelegateCommand<object>(ToMoreInfo, canSubmit);
-        }
 
-        public void ToMoreInfo(object parameter)
-        {
-            Debug.WriteLine(parameter);
-            ActiveMainTab = 1;
+            this.Clients = data.clients;
+            this.Messages = data.messages;
 
-            foreach (var employee in Employees)
-            {
-                if (employee.Name.Equals(parameter))
-                {
-                    ActiveSecondaryTab = Employees.IndexOf(employee);
-                }
-            }
-
-            Debug.WriteLine(ActiveMainTab + " : " + ActiveSecondaryTab);
+            cMoreInfo = new DelegateCommand<object>(MoreInfo, canSubmit);
+            cStartStopSession = new DelegateCommand<object>(StartStopSession, canSubmit);
+            cEmergencyStop = new DelegateCommand<object>(EmergencyStop, canSubmit);
+            cSendMessage = new DelegateCommand<object>(SendMessage, canSubmit);
         }
 
         public bool canSubmit(object parameter)
@@ -47,7 +42,44 @@ namespace DoktersApplicatie
             return true;
         }
 
-       
+        public void MoreInfo(object name)
+        {
+            Debug.WriteLine(name);
+            ActiveMainTab = 1;
+
+            foreach (var client in Clients)
+            {
+                if (client.Name.Equals(name))
+                {
+                    ActiveSecondaryTab = Clients.IndexOf(client);
+                }
+            }
+
+            //Debug.WriteLine(ActiveMainTab + " : " + ActiveSecondaryTab);
+        }
+
+        public void StartStopSession(object parameter)
+        {
+            Debug.WriteLine("Started/Stopped session");
+        }
+
+        public void EmergencyStop(object parameter)
+        {
+            Debug.WriteLine("Emergency Stop");
+        }
+
+        public void SendMessage(object messageBox)
+        {
+            TextBox textbox = (TextBox)messageBox;
+
+            if(textbox.Text != "")
+            {
+                Messages.Add(new Message { Sender = "Doctor", Text = textbox.Text });
+                textbox.Clear();
+                Debug.WriteLine(textbox.Text);
+            }
+            
+        }
 
     }
 }
