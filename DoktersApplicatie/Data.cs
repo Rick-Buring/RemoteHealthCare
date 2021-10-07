4cbec1628ea27c9b1dd9f87dd43aacf129c91add
Leon van der Sar <leon.v.d.sar@gmail.com>
2021-10-07 10:59:37 +0200

    Non-working client panel layout

--- DoktersApplicatie/Data.cs
+++ DoktersApplicatie/Data.cs
@@ -15,10 +15,10 @@ namespace DoktersApplicatie
             clients = new ObservableCollection<Client>();
             messages = new ObservableCollection<Message>();
 
-            clients.Add(new Client { Name = "Kapil Malhotra", Age = 30 });
-            clients.Add(new Client { Name = "Raj Kundra", Age = 34 });
-            clients.Add(new Client { Name = "Amitabh Bachan", Age = 80 });
-            clients.Add(new Client { Name = "Deepak Khanna", Age = 72 });
+            clients.Add(new Client { Name = "Kapil Malhotra", Age = 30, BPM = 52, RPM = 60, KMH = 35.0, CurrWatt = 200, AccWatt = 400, SessionTime = 100, Resistance = 40 });
+            clients.Add(new Client { Name = "Raj Kundra", Age = 34, BPM = 52, RPM = 60, KMH = 35.0, CurrWatt = 200, AccWatt = 400, SessionTime = 100, Resistance = 40 });
+            clients.Add(new Client { Name = "Amitabh Bachan", Age = 80, BPM = 52, RPM = 60, KMH = 35.0, CurrWatt = 200, AccWatt = 400, SessionTime = 100, Resistance = 40 });
+            clients.Add(new Client { Name = "Deepak Khanna", Age = 72, BPM = 52, RPM = 60, KMH = 35.0, CurrWatt = 200, AccWatt = 400, SessionTime = 100, Resistance = 40 });
         }
 
     }
@@ -27,11 +27,19 @@ namespace DoktersApplicatie
     {
         public string Name { get; set; }
         public int Age { get; set; }
+        public int BPM { get; set; }
+        public int RPM { get; set; }
+        public double KMH { get; set; }
+        public int CurrWatt { get; set; }
+        public int AccWatt { get; set; }
+        public int SessionTime { get; set; }
+        public int Resistance { get; set; }
     }
 
     public class Message
     {
         public string Sender { get; set; }
+        public string Receiver { get; set; }
         public string Text { get; set; }
     }
 
