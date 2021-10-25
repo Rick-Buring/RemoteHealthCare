6df1a6a56756276dfa0e99f4b75207b72c73b582
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2021-10-25 13:45:13 +0200

    Bugfix in HistoryData scroll function

--- DoktersApplicatie/HistoryData.cs
+++ DoktersApplicatie/HistoryData.cs
@@ -15,7 +15,7 @@ using LiveCharts.Wpf;
 
 namespace DoktersApplicatie
 {
-    class HistoryData 
+    class HistoryData
     {
         public HistoryValueTimeChart WattHistoryChart { get; set; }
         public HistoryValueTimeChart RpmHistoryChart { get; set; }
@@ -23,15 +23,17 @@ namespace DoktersApplicatie
         public HistoryValueTimeChart KmhHistoryChart { get; set; }
 
         public HistoryData(List<HealthData> healthData) => FillCharts(healthData);
-        
 
-        private void FillCharts (List<HealthData> healthData) {
+
+        private void FillCharts(List<HealthData> healthData)
+        {
             List<HistoryValueTimeChart.ValueTime> watts = new List<HistoryValueTimeChart.ValueTime>();
             List<HistoryValueTimeChart.ValueTime> rpms = new List<HistoryValueTimeChart.ValueTime>();
             List<HistoryValueTimeChart.ValueTime> bpms = new List<HistoryValueTimeChart.ValueTime>();
             List<HistoryValueTimeChart.ValueTime> speeds = new List<HistoryValueTimeChart.ValueTime>();
 
-            foreach (HealthData h in healthData) {
+            foreach (HealthData h in healthData)
+            {
                 watts.Add(new HistoryValueTimeChart.ValueTime(h.CurWatt, h.ElapsedTime));
                 rpms.Add(new HistoryValueTimeChart.ValueTime(h.RPM, h.ElapsedTime));
                 bpms.Add(new HistoryValueTimeChart.ValueTime(h.Heartbeat, h.ElapsedTime));
@@ -46,60 +48,62 @@ namespace DoktersApplicatie
 
         public class HistoryValueTimeChart : INotifyPropertyChanged
         {
-         
-            
-                public Func<double, string> Formatter { get; set; }
-                public ChartValues<ValueTime> Values { get; set; }
-                public DateTime time { get; set; } = DateTime.Now;
-
-                public event PropertyChangedEventHandler PropertyChanged;
-
-                public double From { get; set; }
-                public double To { get; set; }
-
-                private double maxValue { get; set; }
-
-                public int Step { get; set; }
 
 
-                public HistoryValueTimeChart(List<ValueTime> chart)
+            public Func<double, string> Formatter { get; set; }
+            public ChartValues<ValueTime> Values { get; set; }
+            public DateTime time { get; set; } = DateTime.Now;
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public double From { get; set; }
+            public double To { get; set; }
+
+            private double maxValue { get; set; }
+
+            public int Step { get; set; }
+
+
+            public HistoryValueTimeChart(List<ValueTime> chart)
+            {
+                var dayConfig = new CartesianMapper<ValueTime>()
+                    .X(dayModel => dayModel.SecondsSinceStart)
+                    .Y(dayModel => dayModel.Value);
+                this.Values = new ChartValues<ValueTime>(chart);
+                Charting.For<ValueTime>(dayConfig);
+                this.From = 0;
+                this.To = 30;
+
+                this.Step = 10;
+
+                this.maxValue = this.Values[^1].SecondsSinceStart;
+
+                Formatter = value => new System.DateTime((long)(value * TimeSpan.FromSeconds(1).Ticks)).ToString("mm:ss");
+
+            }
+
+            private void updateStep()
+            {
+                this.Step = (int)(this.To - this.From) / 3;
+            }
+
+
+            public void OnMouseWheelScroll(MouseWheelEventArgs e)
+            {
+                Debug.WriteLine(e.Delta);
+                double changeValue = e.Delta / 20.00;
+                if (Keyboard.IsKeyDown(Key.LeftShift))
                 {
-                    var dayConfig = new CartesianMapper<ValueTime>()
-                        .X(dayModel => dayModel.SecondsSinceStart)
-                        .Y(dayModel => dayModel.Value);
-                    this.Values = new ChartValues<ValueTime>(chart);
-                    Charting.For<ValueTime>(dayConfig);
-                    this.From = 0;
-                    this.To = 30;
-
-                    this.Step = 10;
-
-                    this.maxValue = this.Values[^1].SecondsSinceStart;
-
-                    Formatter = value => new System.DateTime((long)(value * TimeSpan.FromSeconds(1).Ticks)).ToString("mm:ss");
-
-                }
-
-                private void updateStep()
-                {
-                    this.Step = (int) (this.To - this.From) / 3;
-                }
-
-
-                public void OnMouseWheelScroll(MouseWheelEventArgs e)
-                {
-                    Debug.WriteLine(e.Delta);
-                    if (Keyboard.IsKeyDown(Key.LeftShift))
+                    if (this.To - changeValue > 0)
                     {
                         this.To -= e.Delta / 20.00;
-                        this.updateStep();
+                    } else {
+                        this.To = 0;
                     }
-                    else
+                    this.updateStep();
+                } else {
+                    if (this.To - changeValue > 0)
                     {
-
-                    double changeValue = e.Delta / 20.00;
-                    
-                    if (this.To - changeValue > 0 ) {
                         this.To -= e.Delta / 20.00;
                     } else {
                         this.To = 0;
@@ -112,11 +116,11 @@ namespace DoktersApplicatie
                         this.From = maxValue;
                     }
 
-                    }
-                } 
+                }
+            }
 
 
-                public class ValueTime
+            public class ValueTime
             {
                 public double SecondsSinceStart { get; set; }
                 public double Value { get; set; }
