ef9ac5f9ff14948adb730206995c5c4e9ba39d9e
Leon van der Sar <leon.v.d.sar@gmail.com>
2021-10-25 15:35:25 +0200

    Not working yet

--- DoktersApplicatie/HistoryVM.cs
+++ DoktersApplicatie/HistoryVM.cs
@@ -1,7 +1,9 @@
 ﻿using CommunicationObjects.DataObjects;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
 
@@ -11,21 +13,36 @@ namespace DoktersApplicatie
     {
         
 
-        public List<HealthData> HealthData { get; set; }
+        public DelegateCommand cRetrieveHistory { get; private set; }
+
+        public List<List<HealthData>> HealthData { get; set; }
         public HealthData LastHealthData { get; set; }
-        public Client SelectedClient { get; }
 
         public HistoryData HistoryData { get; set; }
-
-        
-
-        public HistoryVM(List<HealthData> healthData, Client selectedClient)
+        public HistoryVM(List<List<HealthData>> healthData)
+        public List<History> ClientHistories { get; set; }
+        public History SelectedClientHistory { get; set; }
+        public ObservableCollection<HealthData> Clients { get; private set; }
         {
+            cRetrieveHistory = new DelegateCommand(RetrieveHistory);
+
             this.HealthData = healthData;
-            this.LastHealthData = this.HealthData[this.HealthData.Count - 1];
-            this.SelectedClient = selectedClient;
+
+            this.LastHealthData = this.HealthData[0][this.HealthData[0].Count - 1];
+
+            this.ClientHistories = new List<History>();
+            this.ClientHistories.Add(new History { clientName = "Shaun" });
+            this.ClientHistories.Add(new History { clientName = "Jope" });
+            this.ClientHistories.Add(new History { clientName = "Will" });
+            this.ClientHistories.Add(new History { clientName = "Shilling" });
+            this.ClientHistories.Add(new History { clientName = "Shaquille" });
 
             this.HistoryData = new HistoryData(healthData);
+        }
+
+        public void RetrieveHistory()
+        {
+
         }
 
     }
